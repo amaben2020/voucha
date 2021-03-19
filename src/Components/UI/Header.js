62911bfe3a493081a1c0b38e766163629ddbfa20
amaben2020 <amaben202phantom@gmail.com>
2021-03-19 17:22:57 +0100

    Updated the landing page layout

--- src/Components/UI/Header.js
+++ src/Components/UI/Header.js
@@ -449,27 +449,6 @@ function Header(props) {
 											, {user.name}{" "}
 										</Typography>
 									</Grid>
-
-									<div>
-										<Button
-											aria-controls="simple-menu"
-											aria-haspopup="true"
-											onClick={handleClick}
-										>
-											Open Menu
-										</Button>
-										<Menu
-											id="simple-menu"
-											anchorEl={anchorEl}
-											keepMounted
-											open={Boolean(anchorEl)}
-											onClose={handleClose}
-										>
-											<MenuItem onClick={handleClose}>Profile</MenuItem>
-											<MenuItem onClick={handleClose}>My account</MenuItem>
-											<MenuItem onClick={handleClose}>Logout</MenuItem>
-										</Menu>
-									</div>
 								</Grid>
 								<Grid style={{ marginRight: "20px", marginTop: "28px" }}>
 									<Button
